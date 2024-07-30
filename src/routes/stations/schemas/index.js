e3a11a97458aacec060020c4cb762e0899f199f0
idelapoza <i.delapoza@teamsyste.com>
2024-07-30 16:21:07 +0200

    chore: Add getGasStationByIdSchema and update index.js

--- src/routes/stations/schemas/index.js
+++ src/routes/stations/schemas/index.js
@@ -1,3 +1,4 @@
 module.exports = {
-    getNearesGasStationsSchema: require("./get-nearest-gas-stations")
+    getNearesGasStationsSchema: require("./get-nearest-gas-stations"),
+    getGasStationByIdSchema: require("./get-gas-station-by-id")
 }
