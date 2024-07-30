cc807fb351a2bb09150446649f1a2ac2619b17fe
idelapoza <i.delapoza@teamsyste.com>
2024-07-30 19:02:30 +0200

    feat: Implement GET /stations/location/:location endpoint for retrieving gas stations by location

---
+++ test/cases/routes/stations/get-gas-station-by-location.e2e.test.js
@@ -0,0 +1,120 @@
+const {build} = require("../../helper");
+const insertGasStations = require("./helpers/insert-gas-stations");
+const stations = require("./fixtures/gas-stations");
+
+describe("GET /stations/location/:location", () => {
+  let app;
+  const baseUrl = "api/v1/stations/location";
+
+  beforeAll(async () => {
+    app = await build();
+  });
+
+  beforeEach(async () => {
+    await app.pg.query("TRUNCATE TABLE stations CASCADE");
+  });
+
+  afterAll(async () => {
+    await app.close();
+  });
+
+  it("should return 404 if the station does not exist", async () => {
+    const response = await app.inject({
+      method: "GET",
+      url: `${baseUrl}/qwertyuiop`
+    });
+
+    expect(response.statusCode).toBe(404);
+    expect(response.json()).toHaveProperty("message", "Station not found");
+    expect(response.json()).toHaveProperty("error", "Not Found");
+  });
+
+  it("should return the station by location", async () => {
+    await insertGasStations(app);
+
+    const response = await app.inject({
+      method: "GET",
+      url: `${baseUrl}/${stations[0].localidad}`
+    });
+
+    expect(response.statusCode).toBe(200);
+    expect(Array.isArray(response.json())).toBeTruthy();
+    expect(response.json()).toHaveLength(12);
+  });
+
+  it("should return the station by location with a different case", async () => {
+    await insertGasStations(app);
+
+    const response = await app.inject({
+      method: "GET",
+      url: `${baseUrl}/${stations[0].localidad.toLocaleLowerCase()}`
+    });
+
+    expect(response.statusCode).toBe(200);
+    expect(Array.isArray(response.json())).toBeTruthy();
+    expect(response.json()).toHaveLength(12);
+  });
+
+  it("should return the station by location with accented characters", async () => {
+    await insertGasStations(app);
+
+    const response = await app.inject({
+      method: "GET",
+      url: `${baseUrl}/Jaén`
+    });
+
+    expect(response.statusCode).toBe(200);
+    expect(Array.isArray(response.json())).toBeTruthy();
+    expect(response.json()).toHaveLength(12);
+  });
+
+  it("should return the station by location with accented characters and a different case", async () => {
+    await insertGasStations(app);
+
+    const response = await app.inject({
+      method: "GET",
+      url: `${baseUrl}/jaén`
+    });
+
+    expect(response.statusCode).toBe(200);
+    expect(Array.isArray(response.json())).toBeTruthy();
+    expect(response.json()).toHaveLength(12);
+  });
+
+  it("should return an error if the location is not provided", async () => {
+    const response = await app.inject({
+      method: "GET",
+      url: `${baseUrl}/`
+    });
+
+    expect(response.statusCode).toBe(400);
+    expect(response.json()).toHaveProperty("message", "params/location must NOT have fewer than 2 characters");
+    expect(response.json()).toHaveProperty("error", "Bad Request");
+    expect(response.json()).toHaveProperty("code", "FST_ERR_VALIDATION");
+  });
+
+  it("should return an error if the location is too short", async () => {
+    const response = await app.inject({
+      method: "GET",
+      url: `${baseUrl}/a`
+    });
+
+    expect(response.statusCode).toBe(400);
+    expect(response.json()).toHaveProperty("message", "params/location must NOT have fewer than 2 characters");
+    expect(response.json()).toHaveProperty("error", "Bad Request");
+    expect(response.json()).toHaveProperty("code", "FST_ERR_VALIDATION");
+  });
+
+  it("should return an error if the location is not a string", async () => {
+    const response = await app.inject({
+      method: "GET",
+      url: `${baseUrl}/123`
+    });
+
+    expect(response.statusCode).toBe(400);
+    expect(response.json()).toHaveProperty("message", 'params/location must match pattern "^[a-zA-ZáéíóúÁÉÍÓÚ]+$"');
+    expect(response.json()).toHaveProperty("error", "Bad Request");
+    expect(response.json()).toHaveProperty("code", "FST_ERR_VALIDATION");
+  });
+
+});
