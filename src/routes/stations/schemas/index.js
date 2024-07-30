e93029dfacad0c3239ff1b5993560cadef383393
idelapoza <i.delapoza@teamsyste.com>
2024-07-30 19:59:22 +0200

    feat: Add getGasStationsByMunicipalitySchema for retrieving gas stations by municipality

--- src/routes/stations/schemas/index.js
+++ src/routes/stations/schemas/index.js
@@ -1,5 +1,6 @@
 module.exports = {
     getNearesGasStationsSchema: require("./get-nearest-gas-stations"),
     getGasStationByIdSchema: require("./get-gas-station-by-id"),
-    getGasStationsByLocationSchema: require("./get-gas-stations-by-location")
+    getGasStationsByLocationSchema: require("./get-gas-stations-by-location"),
+    getGasStationsByMunicipalitySchema: require("./get-gas-stations-by-municipality")
 }
