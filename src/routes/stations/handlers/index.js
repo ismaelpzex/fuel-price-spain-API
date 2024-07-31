a2e4e28584e0d5f9f5a2358bd5c6bd5953533b3b
idelapoza <i.delapoza@teamsyste.com>
2024-07-31 21:34:03 +0200

    feat: Add getNearestGasStationsByFuelType handler and endpoint
    The code changes add a new handler `getNearestGasStationsByFuelType` and an endpoint `/stations/nearest/fuel-type` to retrieve the nearest gas stations based on the fuel type. This feature allows users to specify the fuel type, longitude, latitude, and distance to get the nearest gas stations.

--- src/routes/stations/handlers/index.js
+++ src/routes/stations/handlers/index.js
@@ -4,5 +4,6 @@ module.exports = {
     getGasStationsByLocation: require("./get-gas-station-by-location"),
     getGasStationsByMunicipality: require("./get-gas-station-by-municipality"),
     getGasStationsByProvince: require("./get-gas-station-by-province"),
-    getGasStationsByFuelType: require("./get-gas-stations-by-fuel-type")
+    getGasStationsByFuelType: require("./get-gas-stations-by-fuel-type"),
+    getNearestGasStationsByFuelType: require("./get-nearest-gas-stations-by-fuel-type")
 }
