122b5e185e5cb8ab26d6553b5ecebc34d49fc97b
idelapoza <i.delapoza@teamsyste.com>
2024-07-31 17:23:24 +0200

    feat: Add getGasStationsByFuelType schema
    The code changes add a new schema file `get-gas-stations-by-fuel-type.js` to the `stations/schemas` directory. This schema defines the request and response validation for the `/fuel-type/:fuelType` endpoint, allowing users to retrieve gas stations based on the fuel type.

---
+++ src/routes/stations/schemas/get-gas-stations-by-fuel-type.js
@@ -0,0 +1,46 @@
+const allowedFuelTypes = [
+  "biodiesel",
+  "bioetanol",
+  "gas-natural-comprimido",
+  "gas-natural-licuado",
+  "gases-licuados-del-petroleo",
+  "gasoleo-a",
+  "gasoleo-b",
+  "gasoleo-premium",
+  "gasolina-95-e10",
+  "gasolina-95-e5",
+  "gasolina-95-e5-premium",
+  "gasolina-98-e10",
+  "gasolina-98-e5",
+  "hidrogeno"
+];
+
+// Definir el patrón de expresión regular para validar los tipos de combustible
+const fuelTypePattern = allowedFuelTypes.join("|");
+
+module.exports = {
+  params: {
+    type: "object",
+    properties: {
+      fuelType: {
+        type: "string",
+        pattern: `^(${fuelTypePattern})(,(${fuelTypePattern}))*$`,
+        description: `Fuel type(s) to filter by. Allowed values are: ${allowedFuelTypes.join(
+          ", "
+        )}. Multiple values can be separated by commas.`
+      }
+    },
+    required: ["fuelType"]
+  },
+  response: {
+    200: {
+      type: "array",
+      items: {
+        $ref: "station-response-dto"
+      }
+    },
+    400: {$ref: "bad-request"},
+    404: {$ref: "not-found"},
+    500: {$ref: "server-error"}
+  }
+};
