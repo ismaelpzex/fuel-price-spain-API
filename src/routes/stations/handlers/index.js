9571272364e1b91d2c0ae1e4932787670e94124b
idelapoza <i.delapoza@teamsyste.com>
2024-07-31 17:23:14 +0200

    feat: Add getGasStationsByFuelType handler
    The code changes add a new handler function `getGasStationsByFuelType` to the `stations` module. This handler allows users to retrieve gas stations based on the fuel type. It introduces a new endpoint `/fuel-type/:fuelType` to support this functionality.

--- src/routes/stations/handlers/index.js
+++ src/routes/stations/handlers/index.js
@@ -3,5 +3,6 @@ module.exports = {
     getGasStationById: require("./get-gas-station-by-id"),
     getGasStationsByLocation: require("./get-gas-station-by-location"),
     getGasStationsByMunicipality: require("./get-gas-station-by-municipality"),
-    getGasStationsByProvince: require("./get-gas-station-by-province")
+    getGasStationsByProvince: require("./get-gas-station-by-province"),
+    getGasStationsByFuelType: require("./get-gas-stations-by-fuel-type")
 }
